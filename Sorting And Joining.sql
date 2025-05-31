e5f7a70e0bad2d7be6b340e7bc707f745d66b3b3
ebpranto <ehsanulbashar190@gmail.com>
2025-05-31 23:49:43 +0600

    U

--- Sorting And Joining.sql
+++ Sorting And Joining.sql
@@ -79,4 +79,40 @@ SELECT has_5g, AVG(price)
 FROM campusx.smartphones
 GROUP BY has_5g;
 
+-- Group smartphone by brand, and find the brand with the heighest number of  model that have both NFC and IR blaster.
+SELECT brand_name, COUNT(*) AS 'count'
+FROM campusx.smartphones
+WHERE has_nfc = 'True' AND has_ir_blaster = 'True'
+GROUP BY brand_name
+ORDER BY count DESC LIMIT 1;
 
+-- find all samsung 5g enables phone and find out the avg price for NFC and NON-NFC.
+SELECT brand_name, AVG(price), has_nfc
+FROM campusx.smartphones
+WHERE brand_name = 'samsung'
+GROUP BY has_nfc;
+
+
+-- ----------------------------------Having (operation of aggregate group by)--------------------------------
+-- find the avg rating of phone brand which have more than 20 phones.
+SELECT brand_name, AVG(rating) AS 'avg_rating',
+COUNT(*)
+FROM campusx.smartphones
+GROUP BY brand_name
+HAVING COUNT(*) > 40
+ORDER BY avg_rating DESC;
+
+-- Find the top 3 brands with the hightest avg ram that have a  refresh rate of at least 90hz and fast charging available and don't consider brands which have less than 10 phones
+SELECT brand_name,  AVG(ram_capacity) AS 'avg_ram_capasity'
+FROM campusx.smartphones
+WHERE refresh_rate > 90 AND fast_charging_available = 1
+GROUP BY brand_name
+HAVING COUNT(*) > 10
+ORDER BY avg_ram_capasity DESC LIMIT 3;
+
+-- find the avg price of all the phone brands with avg rating > 70 and num_phones more than 10 among all 5g enables phones
+SELECT brand_name, AVG(price), COUNT(*)
+FROM campusx.smartphones
+WHERE has_5g = 'True'
+GROUP BY brand_name
+HAVING AVG(rating) > 70 AND COUNT(*) > 10;
